31bb2ca35844c2f797193ce3b1525a706aac16fe
adelalrafiq <adelalrafiq@gmail.com>
2019-10-25 15:00:32 +0200

    added registration PHP + mySQL

---
+++ register.php
@@ -0,0 +1,41 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+	<h2>Register</h2>
+</div>
+<form method="post" action="register.php">
+	<?php require_once('errors.php'); ?>
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="<?php $username; ?>">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="email" value="<?php $email; ?>">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="password_1">
+	</div>
+	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="password_2">
+	</div>
+	<div class="input-group">
+		<button type="submit" class="btn" name="reg_user">Register</button>
+	</div>
+	<p>
+		Already a member? <a href="login.php">Sign in</a>
+	</p>
+</form>
+</body>
+</html>
